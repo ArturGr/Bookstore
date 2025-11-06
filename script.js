b6f33fed19df1e220d4ab49c34331d8484e16846
ArturGr <arturgroblicki@outlook.de>
2025-11-06 15:01:27 +0100

    Simple cleaning.

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ function init() {
     template_section_book_comments();
 }
 
-function data_import(){
+function data_import() {
     getFromLocalStorage();
     if (myDataLocal.length > 0) {
         for (let i = 0; i < myDataLocal.length; i++) {
-            DATA_ARRAY.push(myDataLocal[i]);    
+            DATA_ARRAY.push(myDataLocal[i]);
         }
     } else {
-        for(let i=0; i < books.length; i++){
+        for (let i = 0; i < books.length; i++) {
             DATA_ARRAY.push(books[i]);
-        }        
+        }
     }
 }
 
@@ -30,59 +30,60 @@ function checking_data() {
     }
 }
 
-function sending_comment(index){
+function sending_comment(index) {
     let COMMENT = document.getElementById(`comment_input_id${index}`);
     if (COMMENT.value.trim() == 0) {
         return console.log("Error: cannot send empty comment.");
     }
     DATA_ARRAY[index].comments.push({
         "name": "Leser",
-        "comment": `${COMMENT.value.trim()}`});
+        "comment": `${COMMENT.value.trim()}`
+    });
     COMMENT.value = "";
     saveData();
     template_table_for_section_book_comments(index);
 }
 
-function saveData(){
-    if(myDataLocal != null){
+function saveData() {
+    if (myDataLocal != null) {
         saveToLocalStorage();
     } else {
-        return console.log("Data saving error");   
+        return console.log("Data saving error");
     }
 }
 
-function saveToLocalStorage(){
+function saveToLocalStorage() {
     localStorage.setItem("myDataLocal", JSON.stringify(DATA_ARRAY));
 }
 
 function getFromLocalStorage() {
     let myData = JSON.parse(localStorage.getItem("myDataLocal"));
-    
-    if(myData == null){
+
+    if (myData == null) {
         return;
     }
     myDataLocal = myData;
 }
 
-function like(index){
+function like(index) {
     DATA_ARRAY[index].liked = true;
     DATA_ARRAY[index].likes += 1;
-    saveData();   
-    template_section_book_details(); 
+    saveData();
+    template_section_book_details();
 }
 
 function unlike(index) {
     const LIKES_AMOUNT = DATA_ARRAY[index].likes;
     DATA_ARRAY[index].liked = false;
     DATA_ARRAY[index].likes = MaxValue(LIKES_AMOUNT);
-    if(FAVORITE_CHECKBOX_REF.checked){
+    if (FAVORITE_CHECKBOX_REF.checked) {
         document.getElementById(`id${index}`).style.display = "none";
     }
     saveData();
     template_section_book_details();
 }
 
-function MaxValue(value){
+function MaxValue(value) {
     const RESULT = value - 1;
     if (RESULT > 0) {
         return RESULT;
@@ -94,29 +95,28 @@ function MaxValue(value){
 const FAVORITE_CHECKBOX_REF = document.getElementById("checkboxID0");
 FAVORITE_CHECKBOX_REF.addEventListener('change', favorite_data_filter);
 
-function favorite_data_filter(){    
-    if(FAVORITE_CHECKBOX_REF.checked){
+function favorite_data_filter() {
+    if (FAVORITE_CHECKBOX_REF.checked) {
         for (let i = 0; i < DATA_ARRAY.length; i++) {
-            if(!DATA_ARRAY[i].liked){
+            if (!DATA_ARRAY[i].liked) {
                 document.getElementById(`id${i}`).style.display = "none";
             }
         }
-    } else{
+    } else {
         for (let i = 0; i < DATA_ARRAY.length; i++) {
-                document.getElementById(`id${i}`).style.display = "";
+            document.getElementById(`id${i}`).style.display = "";
         }
     }
 }
 
 const MAIN_CONTAINER_REF = document.getElementById("main_container");
-MAIN_CONTAINER_REF.addEventListener('keydown', function(event) {
+MAIN_CONTAINER_REF.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         if (event.target.tagName === 'INPUT' && event.target.id.startsWith('comment_input_id')) {
-            event.preventDefault(); 
+            event.preventDefault();
             const indexString = event.target.id.replace('comment_input_id', '');
             const index = parseInt(indexString);
             sending_comment(index);
         }
     }
 });
-
--- scripts/template.js
+++ scripts/template.js
@@ -1,9 +1,9 @@
 function template_main_container() {
     const MAIN_CONTAINER_REF = document.getElementById("main_container");
-    MAIN_CONTAINER_REF.innerHTML="";
+    MAIN_CONTAINER_REF.innerHTML = "";
     for (let i = 0; i < DATA_ARRAY.length; i++) {
-        MAIN_CONTAINER_REF.innerHTML += 
-        `<div id="id${i}" class="book_window">
+        MAIN_CONTAINER_REF.innerHTML +=
+            `<div id="id${i}" class="book_window">
             <section id="book_title_id${i}" class="Book_Title" tabindex="0"></section>
             <section id="book_img_id${i}" class="Book_Img"></section>
             <section id="book_details_id${i}" class="Book_Details" tabindex="0"></section>
@@ -24,8 +24,8 @@ function template_section_book_img() {
     for (let i = 0; i < DATA_ARRAY.length; i++) {
         let section_book_img_ref = document.getElementById(`book_img_id${i}`);
         section_book_img_ref.innerHTML = "";
-        section_book_img_ref.innerHTML += 
-                    `<div>
+        section_book_img_ref.innerHTML +=
+            `<div>
                         <img src="./assets/img/Book.png" alt="Image of book">
                     </div>`;
     }
@@ -40,11 +40,11 @@ function template_section_book_details() {
     }
 }
 
-function template_likes_for_section_book_details(index){
+function template_likes_for_section_book_details(index) {
     let FAVICONE = `<img src="./assets/img/ToLike.png" alt="Image of like icon" tabindex="0" onclick="like(${index})">`;
     if (DATA_ARRAY[index].liked) {
-            FAVICONE = `<img src="./assets/img/ToUnlike.png" alt="Image of like icon" tabindex="0" onclick="unlike(${index})">`;
-        }
+        FAVICONE = `<img src="./assets/img/ToUnlike.png" alt="Image of like icon" tabindex="0" onclick="unlike(${index})">`;
+    }
     return `<div>
                 <p>${DATA_ARRAY[index].price.toFixed(2)} €</p>
                 <div class="Book_Likes">
@@ -54,10 +54,10 @@ function template_likes_for_section_book_details(index){
             </div>`;
 }
 
-function template_table_for_section_book_details(index){
+function template_table_for_section_book_details(index) {
     const section_book_details_table_ref = document.getElementById(`book_details_id${index}`);
-    section_book_details_table_ref.innerHTML += 
-                    `<table>
+    section_book_details_table_ref.innerHTML +=
+        `<table>
                         <tbody>
                             <tr>
                             <th>Author</th>
@@ -79,8 +79,8 @@ function template_section_book_comments() {
     for (let i = 0; i < DATA_ARRAY.length; i++) {
         let section_book_comments_ref = document.getElementById(`book_comments_id${i}`);
         section_book_comments_ref.innerHTML = "";
-        section_book_comments_ref.innerHTML += 
-                    `<div id="book_comments_table_id${i}" class="table"></div>
+        section_book_comments_ref.innerHTML +=
+            `<div id="book_comments_table_id${i}" class="table"></div>
                     <div>
                         <input type="text" id="comment_input_id${i}" placeholder="  Schreibe dein Kommentar ..." size="30">
                         <img src="./assets/img/Sending_icon.png" alt="Image of send icon" tabindex="0" onclick="sending_comment(${i})">
@@ -88,20 +88,20 @@ function template_section_book_comments() {
                     `;
         template_table_for_section_book_comments(i);
     }
-    
+
 }
 
 function template_table_for_section_book_comments(index) {
     const section_book_comments_table_ref = document.getElementById(`book_comments_table_id${index}`);
     section_book_comments_table_ref.innerHTML = "";
-    section_book_comments_table_ref.innerHTML += 
-                    `<table>
+    section_book_comments_table_ref.innerHTML +=
+        `<table>
                         <caption>Kommentare:</caption>
                         <tbody id="comment_table_id${index}"></tbody>
                     </table>`;
     for (let i = 0; i < DATA_ARRAY[index].comments.length; i++) {
         let section_comments_table_ref = document.getElementById(`comment_table_id${index}`)
-        section_comments_table_ref.innerHTML +=`
+        section_comments_table_ref.innerHTML += `
                         <tr>
                             <th>[${DATA_ARRAY[index].comments[i].name}]</th>
                             <td>: ${DATA_ARRAY[index].comments[i].comment}</td>
